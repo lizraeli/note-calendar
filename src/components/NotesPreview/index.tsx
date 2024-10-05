50517ebcf67d4f3290178ea50b48ba421694e7c5
lizraeli <leo2002b@yahoo.com>
2024-10-04 22:39:54 -0400

    streamline styles

--- src/components/NotesPreview/index.tsx
+++ src/components/NotesPreview/index.tsx
@@ -3,25 +3,14 @@ import { useMemo } from 'react';
 import styles from './styles.module.css';
 import { dateFormatterShort } from '../../utils';
 
-function DailyNotesPreview({
-  html,
-  date,
-  isToday,
-}: {
-  html: string;
-  date: Date;
-  isToday: boolean;
-}) {
+function DailyNotesPreview({ html, date }: { html: string; date: Date }) {
   const sanitizedHTML = useMemo(() => DOMPurify.sanitize(html), [html]);
 
   const title = dateFormatterShort.format(date);
 
   return (
     <div className={styles.container}>
-      <div className={styles.date}>
-        {isToday && 'Today, '}
-        {title}
-      </div>
+      <div className={styles.date}>{title}</div>
       <div
         className={styles.html}
         dangerouslySetInnerHTML={{ __html: sanitizedHTML }}
